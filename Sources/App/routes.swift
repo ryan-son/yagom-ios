b5b2af4bb91fa27ac821b3c265069dcf2d9e9934
ryan-son <ryan.son1002@gmail.com>
2021-08-07 14:00:54 +0900

    chore: 프로젝트 설정 - 불필요 파일 및 코드 삭제

--- Sources/App/routes.swift
+++ Sources/App/routes.swift
@@ -1,14 +1,4 @@
 import Fluent
 import Vapor
 
-func routes(_ app: Application) throws {
-    app.get { req in
-        return "It works!"
-    }
-
-    app.get("hello") { req -> String in
-        return "Hello, world!"
-    }
-
-    try app.register(collection: TodoController())
-}
+func routes(_ app: Application) throws { }
